559148d5eb1fae8fb442f878c6553443ef6c5cfb
Szilasi István <szilasii78@gmail.com>
2026-01-12 09:32:30 +0100

    kesz

--- cypress/e2e/dog_api_test.cy.ts
+++ cypress/e2e/dog_api_test.cy.ts
@@ -10,3 +10,30 @@ describe('GET /users végpont tesztelése', () => {
       });
   });
 });
+
+describe('API fájlfeltöltés', () => {
+
+  it('feltölt egy fájlt multipart/form-data kéréssel', () => {
+    
+    const fileName = 'cica.jpg'
+
+    cy.fixture(fileName, 'binary')
+      .then(Cypress.Blob.binaryStringToBlob)
+      .then((fileContent) => {
+        
+        const formData = new FormData();
+        formData.append('avatar', fileContent, fileName);
+
+        cy.request({
+          method: 'POST',
+          url: '/user/avatar',
+          body: formData,
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          }
+        }).then((response) => {
+          expect(response.status).to.eq(200)
+        })
+      })
+  })
+})
--- src/upload/uploadController.ts
+++ src/upload/uploadController.ts
@@ -1,45 +1,64 @@
 import dotenv from "dotenv"
 import config from "../config/config"
-//import fs from "fs"
-import { uploadMiddleware, uploadMiddlewareMultiple } from "../middleware/upload"
-import { File, IFile, IMulterFile } from "../file/file"
 import mysql from "mysql2/promise"
+import { uploadMiddleware, uploadMiddlewareMultiple } from "../middleware/upload"
+import { File, IMulterFile } from "../file/file"
 dotenv.config()
 
 export const getFileList = async (req: any, res: any) => {
     const connection = await mysql.createConnection(config.database);
-    //const uploadPath = config.baseDir + config.uploadDir
+    try {
+        const [results]: any = await connection.query(
+            "Select * from files join userFiles on userFiles.fileId = files.fileId where userFiles.userId = ?", [req.user.userId]
+        );
 
-    const [results]: any = await connection.query(
-        "Select * from files join userFiles on userFiles.fileId = files.fileId where userFiles.userId=? ", [req.user.userId]
-    )
-    if (results.length === 0) {
-        return res.status(200).send("Nincs megjelenítendő adat!")
+        if (results.length === 0) {
+            return res.status(404).send({ message: "Nincs megjelenítendő adat!" })
+        }
+
+        const fileInfos: any[] =[]
+        results.map((file:any) => {
+            fileInfos.push({ name: file.fileName, url: "http://localhost:3000/file/" + file.fileId})
+        })
+
+        res.status(200).send(fileInfos)
+    } catch (err) {
+        console.log(err)
     }
 
-    const fileInfos: any[] = []
-    results.map((file: IFile) => {
-        fileInfos.push({ name: file.fileName, url: "http://localhost:3000/file/" + file.fileId })
-    })
 
 
-    res.status(200).send(fileInfos)
 
+    // const uploadPath = config.baseDir + config.uploadDir
+
+    // fs.readdir(uploadPath, function (err, files) {
+    //     if (err) {
+    //         console.log(err)
+    //         res.status(500).send({ message: "Hiba a fájlok olvasásakor!" })
+    //     }
+    //     const fileInfos: any[] = []
+    //     files.forEach(file => {
+    //         fileInfos.push({ name: file, url: "http://localhost:3000/file/" + file })
+    //     });
+    //     res.status(200).send(fileInfos)
+    // })
 
 
 }
-export const downloadFile = async (req: any, res: any) => {
-    const filename: string = req.params.id
-    const dirPath = config.baseDir + config.uploadDir
+export const downloadFile = async  (req: any, res: any) => {
+    const fileId: string = req.params.id
+
     const connection = await mysql.createConnection(config.database);
     const [results]: any = await connection.query(
-       "Select * from files join userFiles on userFiles.fileId = files.fileId where userFiles.userId = ? and files.fileId = ?", [req.user.userId,filename]
+        "Select fileName from files where fileId=? ", [fileId]
     )
     if (results.length === 0) {
         return res.status(500).send("Nincs meg a file!")
     }
 
-    res.download(dirPath + filename, results[0].fileName, (err: any) => {
+
+    const dirPath = config.baseDir + config.uploadDir
+    res.download(dirPath + fileId, results[0].fileName, (err: any) => {
         if (err) {
             res.status(500).send({
                 error: "A fájl nem tölthető le!" + err
@@ -54,7 +73,8 @@ export const uploadFile = async (req: any, res: any) => {
         if (req.file === undefined) {
             return res.status(400).send({ error: "Töltsön fel fájlt!" })
         }
-        const file: File = new File(req.file, req.user.userId)
+
+        const file = new File(req.file as IMulterFile, req.user.userId)
         await file.saveToDatabase()
 
         res.status(200).send({ message: `A fájl feltöltése sikerült! ${req.file.originalname}` })
@@ -72,14 +92,22 @@ export const uploadFileMultiple = async (req: any, res: any) => {
         if (req.files === undefined) {
             return res.status(400).send({ error: "Töltsön fel fájlokat!" })
         }
-        req.files.map(async (file: IMulterFile) => {
-            const newFile: File = new File(file, req.user.userId)
-            await newFile.saveToDatabase()
 
-        })
+        const badFile: any[] = []
+        await Promise.all(
+            req.files.map(async (file: IMulterFile) => {
+                const newFile = new File(file as IMulterFile, req.user.userId)
 
+                try {
+                    await newFile.saveToDatabase()
+                } catch (err) {
+                    badFile.push(file)
+                }
 
-        res.status(200).send({ message: `A fájl feltöltése sikerült! ${req.files}` })
+            }))
+        const diff = req.files.filter((itemA: any) => !badFile.some(itemB => itemB.filename === itemA.filename))
+        res.status(200).send({ message: `Az alábbi fájlok feltöltése sikerült! `, diff })
+
     }
     catch (err) {
         res.status(500).send({
